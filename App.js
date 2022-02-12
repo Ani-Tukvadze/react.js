9f7aae7443afb75f91f345d50b5c7b3e52f691f9
Ani Tukvadze <ani.tukvadze14@geolab.edu.ge>
2022-02-12 11:23:18 -0800

    added first task

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {Form} from './components/Form'
+
+
+function App() {
+  return (
+    <div>
+      <Form />
+
+    </div>
+  )
+ 
+}
+
+<div id="root"></div>
+export default App;
